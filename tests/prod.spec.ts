d80177e7cb7e525b555940d88f84ce713dfed52a
Jack Lin <03linjac@gmail.com>
2026-04-21 10:21:12 -0400

    Add qualstride.

--- tests/prod.spec.ts
+++ tests/prod.spec.ts
@@ -13,3 +13,8 @@ test('weberify is up', async ({ page }) => {
 	await expect(page.getByRole('link', { name: 'Sign Up' })).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Taking Web Application Testing To The Next Level' })).toBeVisible();
 });
+
+test('qualstride is up', async ({ page }) => {
+  await page.goto('https://weberify.com/');
+	await expect(page).toHaveTitle(/Qualstride/);
+});
