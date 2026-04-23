3c683a1d0ada19398cecc99ee7ece2d1f30818bb
Jack Lin <03linjac@gmail.com>
2026-04-23 17:48:03 -0400

    Use Soft Assertions.

--- tests/prod.spec.ts
+++ tests/prod.spec.ts
@@ -2,26 +2,26 @@ import { test, expect } from '@playwright/test';
 
 test('ironic pop is up', async ({ page }) => {
   await page.goto('https://ironicpop.com/');
-  await expect(page).toHaveTitle(/Ironic Pop/);
-	await expect(page.getByRole('img', { name: 'Ironic Pop' })).toBeVisible();
+  await expect.soft(page).toHaveTitle(/Ironic Pop/);
+	await expect.soft(page.getByRole('img', { name: 'Ironic Pop' })).toBeVisible();
 });
 
 test('weberify is up', async ({ page }) => {
   await page.goto('https://weberify.com/');
-	await expect(page).toHaveTitle(/Welcome to Weberify/);
-  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
-	await expect(page.getByRole('link', { name: 'Sign Up' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Taking Web Application Testing To The Next Level' })).toBeVisible();
+	await expect.soft(page).toHaveTitle(/Welcome to Weberify/);
+  await expect.soft(page.getByRole('link', { name: 'Home' })).toBeVisible();
+	await expect.soft(page.getByRole('link', { name: 'Sign Up' })).toBeVisible();
+  await expect.soft(page.getByRole('heading', { name: 'Taking Web Application Testing To The Next Level' })).toBeVisible();
 });
 
 test('qualstride is up', async ({ page }) => {
   await page.goto('https://qualstride.com/');
-	await expect(page).toHaveTitle(/Qualstride/);
-  await expect(page.locator('#nav-links').getByRole('link', { name: 'The Problem' })).toBeVisible();
-	await expect(page.locator('#nav-links').getByRole('link', { name: 'Results' })).toBeVisible();
-  await expect(page.locator('#nav-links').getByRole('link', { name: 'The Difference' })).toBeVisible();
-	await expect(page.locator('#nav-links').getByRole('link', { name: 'Services' })).toBeVisible();
-  await expect(page.locator('#nav-links').getByRole('link', { name: 'About' })).toBeVisible();
-	await expect(page.locator('#nav-links').getByRole('link', { name: 'Work with Jack' })).toBeVisible();
-	await expect(page.locator('.hero-content')).toBeVisible();
+	await expect.soft(page).toHaveTitle(/Qualstride/);
+  await expect.soft(page.locator('#nav-links').getByRole('link', { name: 'The Problem' })).toBeVisible();
+	await expect.soft(page.locator('#nav-links').getByRole('link', { name: 'Results' })).toBeVisible();
+  await expect.soft(page.locator('#nav-links').getByRole('link', { name: 'The Difference' })).toBeVisible();
+	await expect.soft(page.locator('#nav-links').getByRole('link', { name: 'Services' })).toBeVisible();
+  await expect.soft(page.locator('#nav-links').getByRole('link', { name: 'About' })).toBeVisible();
+	await expect.soft(page.locator('#nav-links').getByRole('link', { name: 'Work with Jack' })).toBeVisible();
+	await expect.soft(page.locator('.hero-content')).toBeVisible();
 });
